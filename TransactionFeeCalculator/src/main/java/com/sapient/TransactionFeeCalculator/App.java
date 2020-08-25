0436964cd178969dcfad91cf3da32c99c83a9305
Jyotsana Mall <jyotsanamall12@gmail.com>
2020-08-25 16:59:10 +0530

    Assignment 2 added

---
+++ TransactionFeeCalculator/src/main/java/com/sapient/TransactionFeeCalculator/App.java
@@ -0,0 +1,19 @@
+package com.sapient.TransactionFeeCalculator;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+public class App {
+
+	private static final String FILE = "transaction.csv";
+
+	public static void main(String[] args) {
+		TransactionIO transactionIO = new TransactionRunner();
+		List<TransactionAttribute> list = transactionIO.read(Path.of(FILE).toFile());
+		TransactionProcessor transactionProcessor = new TransactionProcessor();
+		transactionProcessor.process(list);
+		File report = transactionIO.write(list);
+	}
+
+}
